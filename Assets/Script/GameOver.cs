c1d201f192b11f3ac807b2c0129a62427d01ef03
martinock <martino.aksel.11@gmail.com>
2017-05-21 22:00:18 +0700

    Add games panel

---
+++ Assets/Script/GameOver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+    public GameObject finishedPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (IsGameOver())
+        {
+            finishedPanel.SetActive(true);
+        }
+	}
+
+    private bool IsGameOver()
+    {
+        return (GamesVariables.IsPamanTrue && 
+            GamesVariables.IsPisangTrue &&
+            GamesVariables.IsRambutanTrue &&
+            GamesVariables.IsSayurTrue &&
+            GamesVariables.IsTernakTrue);
+    }
+}
